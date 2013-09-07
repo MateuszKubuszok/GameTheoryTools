df597ca1556b63de82cfbf1e4c3cc85a1a2ef849
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-09-07 15:19:14 +0200

    * Mock headers for libraries.

---
+++ include/gt/gtl.hpp
@@ -0,0 +1,7 @@
+#ifndef __GT_GTL_HPP__
+#define __GT_GTL_HPP__
+
+/* Mock header for libraries  */
+#include "gt/gtl/common.hpp"
+
+#endif /* END #ifndef __GT_GTL_HPP__ */
---
+++ include/gt/model.hpp
@@ -0,0 +1,7 @@
+#ifndef __GT_MODEL_HPP__
+#define __GT_MODEL_HPP__
+
+/* Mock header for libraries  */
+#include "gt/model/common.hpp"
+
+#endif /* END #ifndef __GT_MODEL_HPP__ */
