0b6601d0c9440aa2524c0ffb85b13bdb0065aaf9
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-09-09 18:51:10 +0200

    * Working Root units tests.

--- test/gt/model/root.cpp
+++ test/gt/model/root.cpp
@@ -1,9 +1,42 @@
-#include "gt/model/common.hpp"
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE RootTest
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( my_test )
-{
-  BOOST_CHECK(1);
+#include "gt/model/common.hpp"
+
+class RootTestImpl : public virtual GT::Model::Root {
+public:
+    RootTestImpl() :
+        Root()
+        {}
+
+    virtual GT::Message toString() {
+        return GT::Message("RootTestImpl");
+    }
+};
+
+BOOST_AUTO_TEST_CASE( testIsEqual ) {
+    RootTestImpl root;
+    RootTestImpl root2;
+
+    BOOST_CHECK(root.isEqual(root));
+    BOOST_CHECK(!root.isEqual(root2));
+}
+
+BOOST_AUTO_TEST_CASE( testIsNotNull ) {
+    RootTestImpl root;
+
+    BOOST_CHECK(root.isNotNull());
+}
+
+BOOST_AUTO_TEST_CASE( testIsNull ) {
+    RootTestImpl root;
+
+    BOOST_CHECK(!root.isNull());
+}
+
+BOOST_AUTO_TEST_CASE( testToString ) {
+    RootTestImpl root;
+
+    BOOST_CHECK_EQUAL(root.toString(), GT::Message("RootTestImpl"));
 }
