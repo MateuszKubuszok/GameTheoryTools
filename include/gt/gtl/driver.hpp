cb02c68799a070ed6876a3a6842653c1348c557a
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-09-10 15:51:09 +0200

    * Driver documentation.

--- include/gt/gtl/driver.hpp
+++ include/gt/gtl/driver.hpp
@@ -6,6 +6,11 @@ namespace GTL {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @brief Used for executing actions called Parser.
+ *
+ * @author Mateusz Kubuszok
+ */
 class Driver : public virtual Root {
     CoordinateDriver              coordinate;
     CollectionsDriver<Coordinate> coordinates;
@@ -19,39 +24,110 @@ class Driver : public virtual Root {
     StatementDriver               statement;
 
 public:
+    /**
+     * @brief Default constructor.
+     */
     Driver();
 
+    /**
+     * @brief Default destructor.
+     */
     virtual ~Driver();
 
+    /**
+     * @brief Driver for Coordinate.
+     *
+     * @return CoordinateDriver
+     */
     virtual CoordinateDriver& forCoordinate();
 
+    /**
+     * @brief Driver for Coordinates.
+     *
+     * @return CollectionDriver for Coordinates
+     */
     virtual CollectionsDriver<Coordinate>& forCoordinates();
     
+    /**
+     * @brief Driver for Condition.
+     *
+     * @return ConditionDriver
+     */
     virtual ConditionDriver& forCondition();
 
+    /**
+     * @brief Driver for Conditions.
+     *
+     * @return CollectionDriver for Conditions
+     */
     virtual CollectionsDriver<Condition>& forConditions();
 
+    /**
+     * @brief Driver for Game.
+     *
+     * @return GameDriver
+     */
     virtual GameDriver& forGame();
     
+    /**
+     * @brief Driver for Identifiers.
+     *
+     * @return CollectionDriver for Identifiers
+     */
     virtual CollectionsDriver<Identifier>& forIdentifiers();
     
+    /**
+     * @brief Driver for Objects.
+     *
+     * @return CollectionDriver for Objects
+     */
     virtual CollectionsDriver<Object>& forObjects();
     
+    /**
+     * @brief Driver for Params.
+     *
+     * @return CollectionDriver for Params
+     */
     virtual CollectionsDriver<Param>& forParams();
 
+    /**
+     * @brief Driver for value.
+     *
+     * @return ValueDriver
+     */
     virtual ValueDriver& forValue();
 
+    /**
+     * @brief Driver for Statement.
+     *
+     * @return StatementDriver
+     */
     virtual StatementDriver& forStatement();
 
+    /**
+     * @brief Displays error.
+     *
+     * @param message Message to display
+     */
     virtual void showError(
-        const std::string& message
+        const Message& message
     );
 
+    /**
+     * @brief Driver's Message.
+     *
+     * @return message
+     */
     virtual Message toString();
 }; /* END class Driver */
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @brief Null Driver for handling invalid situations.
+ *
+ * @author Mateusz Kubuszok
+ */
 class NullDriver : public Driver {
     NullCoordinateDriver              coordinate;
     NullCollectionsDriver<Coordinate> coordinates;
@@ -110,7 +186,7 @@ public:
     }
 
     virtual void showError(
-        const std::string& message
+        const Message& message
     ) {}
 
     virtual Message toString() {
