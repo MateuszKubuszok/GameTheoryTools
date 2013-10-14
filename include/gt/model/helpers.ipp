e38e51298a15f4586cf4a2cb11dd2126ea136994
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-10-14 13:37:00 +0200

    * Inlined some of helpers.

---
+++ include/gt/model/helpers.ipp
@@ -0,0 +1,235 @@
+// namespace GT { 
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline Message createMessage(
+    const char* message
+) {
+    return Message(message);
+}
+
+inline Message createMessage(
+    std::string message
+) {
+    return Message(message);
+}
+
+inline Message createMessage(
+    boost::shared_ptr<std::string> message
+) {
+    return Message(*message);
+}
+
+inline MessagePtr createMessagePtr(
+    const char* message
+) {
+    return MessagePtr(new Message(message));
+}
+
+inline MessagePtr createMessagePtr(
+    std::string message
+) {
+    return MessagePtr(new Message(message));
+}
+
+inline MessagePtr createMessagePtr(
+    boost::shared_ptr<std::string> message
+) {
+    return message;
+}
+
+
+inline Messages createMessages() {
+    return Messages();
+}
+
+inline MessagesPtr createMessagesPtr() {
+    return MessagesPtr(new Messages());
+}
+
+inline MessagesPtr createMessagesPtr(
+    Messages messages
+) {
+    return MessagesPtr(new Messages(messages));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline Identifier createIdentifier(
+    const char* identifier
+) {
+    return Identifier(identifier);
+}
+
+inline Identifier createIdentifier(
+    std::string identifier
+) {
+    return Identifier(identifier);
+}
+
+inline Identifier createIdentifier(
+    boost::shared_ptr<std::string> identifier
+) {
+    return Identifier(*identifier);
+}
+
+inline IdentifierPtr createIdentifierPtr(
+    const char* identifier
+) {
+    return IdentifierPtr(new Identifier(identifier));
+}
+
+inline IdentifierPtr createIdentifierPtr(
+    std::string identifier
+) {
+    return IdentifierPtr(new Identifier(identifier));
+}
+
+inline IdentifierPtr createIdentifierPtr(
+    boost::shared_ptr<std::string> identifier   
+) {
+    return identifier;
+}
+
+inline Identifiers createIdentifiers() {
+    return Identifiers();
+}
+
+inline IdentifiersPtr createIdentifiersPtr() {
+    return IdentifiersPtr(new Identifiers());
+}
+
+inline IdentifiersPtr createIdentifiersPtr(
+    Identifiers identifiers
+) {
+    return IdentifiersPtr(new Identifiers(identifiers));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline Number createNumber(
+    int number
+) {
+    return Number(number);
+}
+
+inline Number createNumber(
+    float number
+) {
+    return Number(number);
+}
+
+inline Number createNumber(
+    double number
+) {
+    return Number(number);
+}
+
+inline Number createNumber(
+    std::string number
+) {
+    return Number(number);
+}
+
+inline Number createNumber(
+    boost::shared_ptr<std::string> number
+) {
+    return Number(*number);
+}
+
+inline Number createNumber(
+    Number number
+) {
+    return number;
+}
+
+inline Number createNumber(
+    NumberPtr number
+) {
+    return *number;
+}
+
+inline NumberPtr createNumberPtr(
+    int number
+) {
+    return NumberPtr(new Number(number));
+}
+
+inline NumberPtr createNumberPtr(
+    float number
+) {
+    return NumberPtr(new Number(number));
+}
+
+inline NumberPtr createNumberPtr(
+    double number
+) {
+    return NumberPtr(new Number(number));
+}
+
+inline NumberPtr createNumberPtr(
+    std::string number
+) {
+    return NumberPtr(new Number(number));
+}
+
+inline NumberPtr createNumberPtr(
+    boost::shared_ptr<std::string> number
+) {
+    return NumberPtr(new Number(*number));
+}
+
+inline NumberPtr createNumberPtr(
+    Number number
+) {
+    return NumberPtr(new Number(number));
+}
+
+inline NumberPtr createNumberPtr(
+    NumberPtr number
+) {
+    return number;
+}
+
+inline Numbers createNumbers() {
+    return Numbers();
+}
+
+inline NumbersPtr createNumbersPtr() {
+    return NumbersPtr(new Numbers());
+}
+
+inline NumbersPtr createNumbersPtr(
+    Numbers numbers
+) {
+    return NumbersPtr(new Numbers(numbers));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+inline Positions createPositions() {
+    return Positions();
+}
+
+inline PositionsPtr createPositionsPtr() {
+    return PositionsPtr(new Positions());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace Model {
+    
+////////////////////////////////////////////////////////////////////////////////
+
+inline Players createPlayers() {
+    return Players();
+}
+
+inline PlayersPtr createPlayersPtr() {
+    return PlayersPtr(new Players());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} /* END namespace Model */
+// } /* END nampespace GT*/
--- src/gt/model/helpers.cpp
+++ src/gt/model/helpers.cpp
@@ -13,24 +13,6 @@ Message createMessage(
 }
 
 Message createMessage(
-    const char* message
-) {
-    return Message(message);
-}
-
-Message createMessage(
-    std::string message
-) {
-    return Message(message);
-}
-
-Message createMessage(
-    boost::shared_ptr<std::string> message
-) {
-    return Message(*message);
-}
-
-Message createMessage(
     Number number
 ) {
     std::stringstream result;
@@ -56,24 +38,6 @@ MessagePtr createMessagePtr(
 }
 
 MessagePtr createMessagePtr(
-    const char* message
-) {
-    return MessagePtr(new Message(message));
-}
-
-MessagePtr createMessagePtr(
-    std::string message
-) {
-    return MessagePtr(new Message(message));
-}
-
-MessagePtr createMessagePtr(
-    boost::shared_ptr<std::string> message
-) {
-    return message;
-}
-
-MessagePtr createMessagePtr(
     Number number
 ) {
     std::stringstream result;
@@ -89,20 +53,6 @@ MessagePtr createMessagePtr(
     return MessagePtr(new Message(result.str()));
 }
 
-Messages createMessages() {
-    return Messages();
-}
-
-MessagesPtr createMessagesPtr() {
-    return MessagesPtr(new Messages());
-}
-
-MessagesPtr createMessagesPtr(
-    Messages messages
-) {
-    return MessagesPtr(new Messages(messages));
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 
 Identifier createIdentifier(
@@ -113,24 +63,6 @@ Identifier createIdentifier(
     return Identifier(result.str());
 }
 
-Identifier createIdentifier(
-    const char* identifier
-) {
-    return Identifier(identifier);
-}
-
-Identifier createIdentifier(
-    std::string identifier
-) {
-    return Identifier(identifier);
-}
-
-Identifier createIdentifier(
-    boost::shared_ptr<std::string> identifier
-) {
-    return Identifier(*identifier);
-}
-
 IdentifierPtr createIdentifierPtr(
     Index identifier
 ) {
@@ -138,166 +70,6 @@ IdentifierPtr createIdentifierPtr(
     result << identifier;
     return IdentifierPtr(new Identifier(result.str()));
 }
-
-IdentifierPtr createIdentifierPtr(
-    const char* identifier
-) {
-    return IdentifierPtr(new Identifier(identifier));
-}
-
-IdentifierPtr createIdentifierPtr(
-    std::string identifier
-) {
-    return IdentifierPtr(new Identifier(identifier));
-}
-
-IdentifierPtr createIdentifierPtr(
-    boost::shared_ptr<std::string> identifier	
-) {
-    return identifier;
-}
-
-Identifiers createIdentifiers() {
-    return Identifiers();
-}
-
-IdentifiersPtr createIdentifiersPtr() {
-    return IdentifiersPtr(new Identifiers());
-}
-
-IdentifiersPtr createIdentifiersPtr(
-    Identifiers identifiers
-) {
-    return IdentifiersPtr(new Identifiers(identifiers));
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-Number createNumber(
-    int number
-) {
-    return Number(number);
-}
-
-Number createNumber(
-    float number
-) {
-    return Number(number);
-}
-
-Number createNumber(
-    double number
-) {
-    return Number(number);
-}
-
-Number createNumber(
-    std::string number
-) {
-    return Number(number);
-}
-
-Number createNumber(
-    boost::shared_ptr<std::string> number
-) {
-    return Number(*number);
-}
-
-Number createNumber(
-    Number number
-) {
-    return number;
-}
-
-Number createNumber(
-    NumberPtr number
-) {
-    return *number;
-}
-
-NumberPtr createNumberPtr(
-    int number
-) {
-    return NumberPtr(new Number(number));
-}
-
-NumberPtr createNumberPtr(
-    float number
-) {
-    return NumberPtr(new Number(number));
-}
-
-NumberPtr createNumberPtr(
-    double number
-) {
-    return NumberPtr(new Number(number));
-}
-
-NumberPtr createNumberPtr(
-    std::string number
-) {
-    return NumberPtr(new Number(number));
-}
-
-NumberPtr createNumberPtr(
-    boost::shared_ptr<std::string> number
-) {
-    return NumberPtr(new Number(*number));
-}
-
-NumberPtr createNumberPtr(
-    Number number
-) {
-    return NumberPtr(new Number(number));
-}
-
-NumberPtr createNumberPtr(
-    NumberPtr number
-) {
-    return number;
-}
-
-Numbers createNumbers() {
-    return Numbers();
-}
-
-NumbersPtr createNumbersPtr() {
-    return NumbersPtr(new Numbers());
-}
-
-NumbersPtr createNumbersPtr(
-    Numbers numbers
-) {
-    return NumbersPtr(new Numbers(numbers));
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-Positions createPositions() {
-    return Positions();
-}
-
-PositionsPtr createPositionsPtr() {
-    return PositionsPtr(new Positions());
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-namespace Model {
-    
-////////////////////////////////////////////////////////////////////////////////
-
-Players createPlayers() {
-    return Players();
-}
-
-PlayersPtr createPlayersPtr() {
-    return PlayersPtr(new Players());
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-} /* END namespace Model */
 
 ////////////////////////////////////////////////////////////////////////////////
 
