383a0af9b0140a6b9f9db68ff465fdf2903f3f48
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2013-10-30 01:19:53 +0100

    * ExecutionDriver passes tests, * CheckingDriver test variable rename.

--- test/gt/gtl/checking_driver.cpp
+++ test/gt/gtl/checking_driver.cpp
@@ -10,19 +10,19 @@ BOOST_AUTO_TEST_CASE( CheckingDriver_showError ) {
         .setBuilderMode(GT::Model::ResultBuilderMode::PLAIN)
         .setIndentationMode(GT::Model::ResultIndentationMode::TABS);
 
-    std::ostringstream outputStream;
+    std::ostringstream errorStream;
     GT::GTL::InputLocationPtr location = GT::GTL::NullFactory::getInstance().createInputLocation();
     GT::Message               message  = GT::Message("Error Message");
     GT::GTL::ObjectPtr        object   = GT::GTL::ErrorFactory::getInstance().createObject("Invalid Object");
 
     // when
-    GT::GTL::CheckingDriver driver(&outputStream);
+    GT::GTL::CheckingDriver driver(&errorStream);
     driver.showError(*location, message);
     driver.showError(*object);
 
     // then
     BOOST_CHECK_EQUAL(
-        outputStream.str(),
+        errorStream.str(),
         GT::Message() +
         "Error:\n"
         "\tError Message\n"
---
+++ test/gt/gtl/execution_driver.cpp
@@ -0,0 +1,26 @@
+#include "gt/gtl/test_common.hpp"
+
+BOOST_AUTO_TEST_SUITE( ExecutionDriver )
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_CASE( ExecutionDriver_showResult ) {
+    // given
+    std::ostringstream outputStream;
+    GT::GTL::ResultPtr result = GT::GTL::ResultFactory::getInstance().constResult(GT::Message("Result"));
+
+    // when
+    GT::GTL::ExecutionDriver driver(&outputStream, &outputStream);
+    driver.showResult(result);
+
+    // then
+    BOOST_CHECK_EQUAL(
+        outputStream.str(),
+        GT::Message() +
+        "Result"
+    );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+BOOST_AUTO_TEST_SUITE_END()
